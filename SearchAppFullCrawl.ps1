cbb46994b1197b7f3f4ffdeb9df43fe6c02c04ae
Purushottam Gupta <purushottam.2014@vit.ac.in>
2020-08-12 17:50:44 +0530

    Add files via upload
    To make sure you can start crawl on remote server, you need to do additional changes. Follow this link for updates: https://sharepoint.stackexchange.com/questions/264346/how-to-execute-powershell-script-from-server1-for-deploying-wsp-in-server2

---
+++ SearchAppFullCrawl.ps1
@@ -0,0 +1,39 @@
+﻿$jobuserpswd = "5oup+ime"
+$password = ConvertTo-SecureString $jobuserpswd -AsPlainText -Force
+$username = "Dv\svcdvspsetup"
+$dvauthservername = "dvspapp1"
+$cred = New-Object System.Management.Automation.PSCredential($username,$password)
+
+try{
+Invoke-Command -ComputerName $dvauthservername -Credential $cred -Authentication Credssp -ScriptBlock{
+Add-PSSnapin Microsoft.SharePoint.Powershell -ErrorAction SilentlyContinue
+$SearchServiceApplication = Get-SPEnterpriseSearchServiceApplication -Identity "Search Service Application (Pub)" 
+
+#Get the Local SharePoint sites content source  
+$ContentSource = $SearchServiceApplication | Get-SPEnterpriseSearchCrawlContentSource -Identity "Community"  
+  
+  #Check if Crawler is not already running or Idle  
+  if($ContentSource.CrawlState -eq "Idle")  
+        {  
+            Write-Host "Full Crawl Starts..." -ForegroundColor Green            
+            $ContentSource.StartFullCrawl()
+            
+            Do{
+            Write-Host "`r#" -NoNewline -ForegroundColor Yellow
+            Start-Sleep 5
+            } while ($ContentSource.CrawlState -ne "Idle")
+            
+            Write-Host "`Full Crawl Completed" -ForegroundColor Green
+              
+       }  
+  else  
+  {  
+   Write-Host "Full Crawl is already Started!"  
+   Write-Host "NAME: ", $ContentSource.Name, " - ", $ContentSource.CrawlStatus, $ContentSource.CrawlState -ForegroundColor Yellow
+  }}
+  }
+ catch [Exception]{
+Write-Error "Error while running script"
+$exceptionMessage = ($_.Exception|format-list -force)
+Write-Debug $exceptionMessage.ToString()
+}
